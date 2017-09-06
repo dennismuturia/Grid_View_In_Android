fc13096785255909f1b2461cc4ae33c810e80b35
dennismuturia <dmuturia2922@gmail.com>
2017-09-06 10:44:18 +0300

    Here i have initialized the gridview as well as an array of letters

--- app/src/main/java/com/example/dennis/grid/MainActivity.java
+++ app/src/main/java/com/example/dennis/grid/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.dennis.grid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Initialization of grid view
+    GridView gridView;
+    //Initialize a new array
+    String[] letters = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+            "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
